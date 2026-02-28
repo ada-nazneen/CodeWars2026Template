432121b0908f97e28ba219ccd4149af9b6b81b5c
IdiotStinker <saltylightning564@gmail.com>
2026-02-28 18:14:58 +0000

    Changed!!!

--- references/useable/numberSuffix.py
+++ references/useable/numberSuffix.py
@@ -1,4 +1,5 @@
 def numberSuffix(number):
+    if number % 100 in [11, 12, 13]: return str(number) + "th"
     if number % 10 == 1: return str(number) + "st"
     if number % 10 == 2: return str(number) + "nd"
     if number % 10 == 3: return str(number) + "rd"
