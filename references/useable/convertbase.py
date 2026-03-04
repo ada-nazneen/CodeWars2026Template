3652d649b4c1231568e4863c82f5fa91aba3d9b5
ada_nazneen <ada.nazneen.m@gmail.com>
2026-03-04 02:06:23 +0000

    modified:   references/useable/convertbase.py

--- references/useable/convertbase.py
+++ references/useable/convertbase.py
@@ -1,3 +1,4 @@
+#converts from base start to base final. 
 def convertbase(string, bStart, bFinal):
     alpha = "0123456789abcdefghijklmnopqrstuvwxyz"
     decimal = int(string, bStart)
@@ -10,4 +11,5 @@ def convertbase(string, bStart, bFinal):
 
 if __name__ == "__main__":
     print(convertbase("code", 36, 35))
+    #converts from base 8 to base 10 for ex
     print(convertbase("357", 8, 10))
